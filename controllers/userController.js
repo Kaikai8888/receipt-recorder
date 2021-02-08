1d104057cf81920ef1c41106ae590cd44c5a0ecf
Kaikai8888 <wuky812@gmail.com>
2021-02-08 13:29:45 +0800

    fix: check email case-sensitively in login route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,16 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { User } = require('../models')
+const { User, sequelize } = require('../models')
 const redis = require('../config/redis.js')
 
 module.exports = {
   async signIn(req, res, next) {
     try {
       const { email, password } = req.body
-      const user = await User.findOne({ where: { email } })
-      if (!user) return res.status(400).json({ status: 'error', message: 'The email hasn\'t been registered' })
-      if (!bcrypt.compareSync(password, user.password)) return res.status(400).json({ status: 'error', message: 'Wrong email or password.' })
+      const result = await sequelize.query(`SELECT * FROM Users WHERE BINARY Users.email=:email LIMIT 1`, { replacements: { email }, type: sequelize.QueryTypes.SELECT })
+      const user = result[0]
+      if (!user) return res.status(403).json({ status: 'error', message: 'The email hasn\'t been registered' })
+      if (!bcrypt.compareSync(password, user.password)) return res.status(403).json({ status: 'error', message: 'Wrong email or password.' })
 
       const payload = {
         id: user.id,
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,17 @@ const passport = require('../config/passport.js')
 const userController = require('../controllers/userController.js')
 const tagController = require('../controllers/tagController.js')
 const { signinCheck, tagCheck, paramsCheck } = require('../middlewares/validator.js')
-const authenticator = passport.authenticate('jwt', { session: false })
+const authenticate = passport.authenticate('jwt', { session: false })
 
 //all path starts with prefix '/api', which has already been set in app.js file
 router.post('/signin', signinCheck, userController.signIn)
-router.post('/signout', authenticator, userController.signOut)
+router.post('/signout', authenticate, userController.signOut)
 
-router.get('/tags', authenticator, tagController.getTags)
-router.get('/tags/:id', authenticator, paramsCheck, tagController.getTag)
-router.post('/tags', authenticator, tagCheck, tagController.createTag)
-router.put('/tags/:id', authenticator, paramsCheck, tagCheck, tagController.editTag)
-router.delete('/tags/:id', authenticator, paramsCheck, tagController.deleteTag)
+router.get('/tags', authenticate, tagController.getTags)
+router.get('/tags/:id', authenticate, paramsCheck, tagController.getTag)
+router.post('/tags', authenticate, tagCheck, tagController.createTag)
+router.put('/tags/:id', authenticate, paramsCheck, tagCheck, tagController.editTag)
+router.delete('/tags/:id', authenticate, paramsCheck, tagController.deleteTag)
 
 module.exports = router
 
