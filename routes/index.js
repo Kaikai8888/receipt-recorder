bad5b91cb5a1973e1a9356f35054ed9589d4ce90
Kaikai8888 <wuky812@gmail.com>
2021-02-10 12:54:34 +0800

    feat: upload receipt file in POST /receipts route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 const router = express.Router()
 const passport = require('../config/passport.js')
 const userController = require('../controllers/userController.js')
 const tagController = require('../controllers/tagController.js')
+const receiptController = require('../controllers/receiptController.js')
 const { signinCheck, tagCheck, paramsCheck } = require('../middlewares/validator.js')
 const authenticate = passport.authenticate('jwt', { session: false })
 
@@ -15,6 +18,8 @@ router.get('/tags/:id', authenticate, paramsCheck, tagController.getTag)
 router.post('/tags', authenticate, tagCheck, tagController.createTag)
 router.put('/tags/:id', authenticate, paramsCheck, tagCheck, tagController.editTag)
 router.delete('/tags/:id', authenticate, paramsCheck, tagController.deleteTag)
+
+router.post('/receipts', authenticate, upload.single('receipt'), receiptController.createReceipt)
 
 module.exports = router
 
